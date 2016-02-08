7ee56253fdcfd4f5b6e328a9277786773e316662
MD. SHAHAB UDDIN <support@devsit.com>
2016-02-08 22:04:35 +0600

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Theme customizations
+ *
+ * @package      ThemeName
+ * @author       CoderTunes
+ * @link         http://www.codertunes.com/
+ * @copyright    Copyright (c) 2016, CoderTunes
+ * @license      GPL-2.0+
+ */
+
+// Load child theme textdomain.
+load_child_theme_textdomain( 'textdomain' );
+
+add_action( 'genesis_setup', 'scratch_setup' );
+/**
+ * Theme setup.
+ *
+ * Attach all of the site-wide functions to the correct hooks and filters. All
+ * the functions themselves are defined below this setup function.
+ *
+ * @since 1.0.0
+ */
+function scratch_setup() {
+
+}
