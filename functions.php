f18f7c0558f9e363f33d5102fca7bd9bf416e0fd
MD. SHAHAB UDDIN <support@devsit.com>
2016-02-08 22:45:07 +0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -23,8 +23,29 @@ add_action( 'genesis_setup', 'themeName_setup' );
  * @since 1.0.0
  */
 function themeName_setup() {  
+  
   // Define theme constants.
-	define( 'CHILD_THEME_NAME', 'ThemeName' );
-	define( 'CHILD_THEME_URL', 'https://github.com/engrshahabuddin/Genesis-Child-Theme' );
-	define( 'CHILD_THEME_VERSION', '1.0.0' );
+   define( 'CHILD_THEME_NAME', 'ThemeName' );
+   define( 'CHILD_THEME_URL', 'https://github.com/engrshahabuddin/Genesis-Child-Theme' );
+   define( 'CHILD_THEME_VERSION', '1.0.0' );
+   
+   // Add HTML5 markup structure.
+    add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption'  ) );
+	
+   // Add viewport meta tag for mobile browsers.
+    add_theme_support( 'genesis-responsive-viewport' );
+	
+   // Add theme support for accessibility.
+    add_theme_support( 'genesis-accessibility', array(
+	'404-page',
+	'drop-down-menu',
+	'headings',
+	'rems',
+	'search-form',
+	'skip-links',
+      ) );
+
+     // Add theme support for footer widgets.
+      add_theme_support( 'genesis-footer-widgets', 3 );
+	
 }
