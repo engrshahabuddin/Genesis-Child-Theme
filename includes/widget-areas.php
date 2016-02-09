4fe03deba73f86b91f62dfeaf413b47f7dca9c52
MD. SHAHAB UDDIN <support@devsit.com>
2016-02-09 14:02:26 +0600

    Update widget-areas.php

--- includes/widget-areas.php
+++ includes/widget-areas.php
@@ -12,11 +12,11 @@
 // Register front page widget areas
 genesis_register_sidebar( array(
 	'id'            => 'home-welcome',
-	'name'          => __( 'Home Welcome', 'scratch' ),
-	'description'   => __( 'This is a home widget area that will show on the front page', 'scratch' ),
+	'name'          => __( 'Home Welcome', 'themeName' ),
+	'description'   => __( 'This is a home widget area that will show on the front page', 'themeName' ),
 ) );
 genesis_register_sidebar( array(
 	'id'            => 'call-to-action',
-	'name'          => __( 'Call to Action', 'scratch' ),
-	'description'   => __( 'This is a call to action widget area that will show on the front page', 'scratch' ),
+	'name'          => __( 'Call to Action', 'themeName' ),
+	'description'   => __( 'This is a call to action widget area that will show on the front page', 'themeName' ),
 ) );
