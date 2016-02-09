eda1c5a178779684703fe405bc5601eb0db3ebb8
MD. SHAHAB UDDIN <support@devsit.com>
2016-02-09 13:47:28 +0600

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -10,7 +10,14 @@
  * @license      GPL-2.0+
  */
 
-function themeName_home_page_setup() {
+add_action( 'genesis_meta', 'scratch_home_page_setup' );
+/**
+ * Set up the homepage layout by conditionally loading sections when widgets
+ * are active.
+ *
+ * @since 1.0.0
+ */
+function scratch_home_page_setup() {
 
 	$home_sidebars = array(
 		'home_welcome' 	   => is_active_sidebar( 'home-welcome' ),
@@ -24,13 +31,44 @@ function themeName_home_page_setup() {
 
 	// Add home welcome area if "Home Welcome" widget area is active.
 	if ( $home_sidebars['home_welcome'] ) {
-		//
-	}
-	// Add call to action area if "Call to Action" widget area is active.
-	if ( $home_sidebars['call_to_action'] ) {
-		//
+		add_action( 'genesis_after_header', 'scratch_add_home_welcome' );
 	}
 
+	// Add call to action area if "Call to Action" widget area is active.
+	if ( $home_sidebars['call_to_action'] ) {
+		add_action( 'genesis_after_header', 'scratch_add_call_to_action' );
+	}
+
+}
+
+/**
+ * Display content for the "Home Welcome" section.
+ *
+ * @since 1.0.0
+ */
+function scratch_add_home_welcome() {
+
+	genesis_widget_area( 'home-welcome',
+		array(
+			'before' => '<div class="home-welcome"><div class="wrap">',
+			'after' => '</div></div>',
+		)
+	);
+}
+
+/**
+ * Display content for the "Call to Action" section.
+ *
+ * @since 1.0.0
+ */
+function scratch_add_call_to_action() {
+
+	genesis_widget_area( 'call-to-action',
+		array(
+			'before' => '<div class="call-to-action"><div class="wrap">',
+			'after' => '</div></div>',
+		)
+	);
 }
 
 genesis();
