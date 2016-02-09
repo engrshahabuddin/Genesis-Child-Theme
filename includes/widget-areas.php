33059ce1a9aa3d6c6e4aa81f245acf9aea5b7d94
MD. SHAHAB UDDIN <support@devsit.com>
2016-02-09 12:47:41 +0600

    Create widget-areas.php

---
+++ includes/widget-areas.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Register widget areas
+ *
+ * @package      themeName
+ * @author       DevsIT Limited
+ * @link         http://www.devsit.com/
+ * @copyright    Copyright (c) 2016, DevsIT Limited
+ * @license      GPL-2.0+
+ */
+
+// Register front page widget areas
+genesis_register_sidebar( array(
+	'id'            => 'home-welcome',
+	'name'          => __( 'Home Welcome', 'scratch' ),
+	'description'   => __( 'This is a home widget area that will show on the front page', 'scratch' ),
+) );
+genesis_register_sidebar( array(
+	'id'            => 'call-to-action',
+	'name'          => __( 'Call to Action', 'scratch' ),
+	'description'   => __( 'This is a call to action widget area that will show on the front page', 'scratch' ),
+) );
